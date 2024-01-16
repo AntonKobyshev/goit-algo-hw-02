256e8d7e0328a6c2d2997c0efc9ce5f3210d0ad1
Anton Kobyshev <antonkobyshev@gmail.com>
2024-01-17 00:31:17 +0100

    Update task02.py

--- task02.py
+++ task02.py
@@ -1,16 +1,12 @@
 from collections import deque
 
 def is_palindrome(input_str):
-    # Функція для визначення, чи є рядок паліндромом
 
-    # Видаляємо пробіли та переводимо все в нижній регістр
     input_str = input_str.replace(" ", "").lower()
 
-    # Створюємо deque з рядка
     char_deque = deque(input_str)
 
     while len(char_deque) > 1:
-        # Порівнюємо перший та останній символи
         if char_deque.popleft() != char_deque.pop():
             return False
 
